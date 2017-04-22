1f3ddbe8afd8cf642b58ee1c43f9b398ba7339e0
sonro <sonro@gmx.com>
2017-04-22 21:06:52 +0100

    base package

--- gulpfile.js
+++ gulpfile.js
@@ -7,46 +7,54 @@ var gulp    = require('gulp'),
 gulp.task('default', ['serve', 'watch']);
 
 gulp.task('markup', function() {
-    gulp.src('./src/*.php)
-        .pipe(gulp.dest('./html/'));
+    gulp.src('./src/**/*.php')
+        .pipe(gulp.dest('./html'))
     
-    gulp.src('./src/*.html)
-        .pipe(gulp.dest('./html/'));
+    gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./html'))
+
+         bSync.reload();
+
 });
 
 gulp.task('styles', function() {
-    gulp.src('./src/assets/app.scss')
+    gulp.src('./src/assets/scss/app.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./html/assets/css/app.css')
+        .pipe(gulp.dest('./html/assets/css'))
         .pipe(bSync.reload({stream:true}));
 });
 
 gulp.task('scripts', function() {
     gulp.src('./src/assets/js/**/*.js')
-        .pipe(gulp.dest('./html/assets/js');
+        .pipe(plumber())
+        .pipe(gulp.dest('./html/assets/js'));
 });
 
 
 gulp.task('serve', ['markup', 'styles', 'scripts'], function() {
     bSync.init({
         open: 'external',
-        host: 'local.dev',
-        proxy: 'local.dev',
-        port: 80
+        host: 'localhost',
+        proxy: 'localhost',
+        // port: 80
         // server: {
-        //     basedir: "./"
+        //     basedir: "html"
         // }
     });
 });
 
-gulp.task('watch' function() {
+gulp.task('reload', function() {
+    bSync.reload();
+});
+
+gulp.task('watch', function() {
     // html/php files
-    gulp.watch('./src/*.html', ['markup']); 
-    gulp.watch('./src/*.php', ['markup']);
+    gulp.watch('./src/**/*.html', ['markup']); 
+    gulp.watch('./src/**/*.php', ['markup']);
 
     // sass
-    gulp.watch('./src/scss/**/*.scss', ['styles']);
+    gulp.watch('./src/assets/scss/**/*.scss', ['styles']);
 
     // js
-    gulp.watch('./src/assets/js/**/*.js', gSync(['scripts', 'reload'], "script build"));
+    gulp.watch('./src/assets/js/**/*.js', gSync.sync(['scripts', 'reload'], "script build"));
 });
--- src/index.php
+++ src/index.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/css/app.css">
-    <title>php</title>
-</head>
-<body>
-    <h1>woohoo</h1>
-    <script src="assets/js/app.js"></script>
-</body>
-</html>
+<?php
+readfile('./includes/mainHead.html');
+?>
+
+    <h1>php</h1>
+
+<?php
+readfile('./includes/mainFoot.html');
+?>
